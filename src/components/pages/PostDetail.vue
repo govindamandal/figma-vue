f3517747932bf30bab25d49e5f758626700d58be
Govinda Mandal <govinda4india@gmail.com>
2023-05-17 14:39:07 +0530

    Added user section and post content

--- src/components/pages/PostDetail.vue
+++ src/components/pages/PostDetail.vue
@@ -1,3 +1,44 @@
 <template>
-  <h1>Post Details</h1>
+  <div class='flex max-w-xl bg-white mx-auto'>
+    <div class='flex items-center w-full'>
+      <div class='w-full'>
+        <div class="flex flex-row mt-2 px-2 py-3 mx-3">
+          <div class="w-auto h-auto rounded-full border-2 border-pink-500">
+            <img class='w-12 h-12 object-cover rounded-full shadow cursor-pointer' alt='User avatar' src='https://en.gravatar.com/userimage/100383909/61e19fd27a19e420a4251098c3939def.png'>
+          </div>
+          <!-- Editor -->
+          <div class="flex flex-col mb-2 ml-4 mt-1">
+            <div class='text-gray-600 text-sm font-semibold'>
+              <span>안녕 나 응애</span>
+            </div>
+            <div class='flex w-full mt-1'>
+              <div class='text-slate-500 font-base text-xs mr-1 cursor-pointer'>
+                165cm . 53kg
+              </div>
+            </div>
+          </div>
+          <!-- Time of the post -->
+          <div class="flex flex-row mb-2 ml-4 mt-1">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="w-4 h-4 bg-green-700 rounded-lg text-white font-bold">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+            </svg>
+            <span class='text-slate-500 font-thin text-xs ml-2'>
+              1일전
+            </span>
+          </div>
+        </div>
+        
+        <div class='text-gray-600 font-semibold text-lg mb-2 mx-3 px-2'>
+          지난 월요일에 했던 이벤트 중 가장 괜찮은 상품 뭐야?
+        </div>
+        <div class='text-gray-500 font-thin text-sm mb-6 mx-3 px-2'>
+          <p class="mt-2">지난 월요일에 2023년 S/S 트렌드 알아보기 이벤트 참석했던 팝들아~ 혹시 어떤 상품이 제일 괜찮았어?</p>
+          <p class="mt-2">후기 올라오는거 보면 로우라이즈? 그게 제일 반응 좋고 그 테이블이 제일 재밌었다던데 맞아???</p>
+          <p class="mt-2">올해 로우라이즈가 트렌드라길래 나도 도전해보고 싶은데 말라깽이가 아닌 사람들도 잘 어울릴지 너무너무 궁금해ㅜㅜ로우라이즈 테이블에 있었던 팝들 있으면 어땠는지 후기 좀 공유해주라~~!</p>
+        </div>
+
+        <div class='text-gray-400 font-medium text-sm mb-7 mt-6 mx-3 px-2'><img class="rounded" src="https://picsum.photos/536/354"></div>
+      </div>
+    </div>
+  </div>
 </template>
