baceee44240dc70e9b1093566979d5bef2c53597
Govinda Mandal <govinda4india@gmail.com>
2023-05-17 13:35:17 +0530

    Added footer

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import Header from './components/layout/Header.vue';
 import Footer from './components/layout/Footer.vue';
-import Home from './components/pages/Home.vue'
+import PostDetail from './components/pages/PostDetail.vue'
 </script>
 
 <template>
   <div class="container mx-auto">
     <Header />
+    <PostDetail />
+    <Footer />
   </div>
 </template>
 
--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,3 +1,9 @@
 <template>
-  <h1>Footer</h1>
+  <footer
+  class="bg-neutral-200 text-center dark:bg-neutral-700 lg:text-left">
+    <div class="p-4 text-center text-neutral-700 dark:text-neutral-200">
+      © 2023 Copyright:
+      <a class="text-neutral-800 dark:text-neutral-400" href="https://hyperhire.in/en/">Hiperhire</a>
+    </div>
+  </footer>
 </template>
